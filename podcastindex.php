3a2cb23893ea206d137105041d98e55fd206b430
Andy Lehman <Andy@lehmancreations.com>
2021-02-15 15:20:43 -0500

    V1.2 Added support for podcast season and episode, as well as location tag

--- podcastindex.php
+++ podcastindex.php
@@ -4,7 +4,7 @@
 Plugin Name: Podcast Namespace
 Plugin URI: https://github.com/Lehmancreations/Podcast-Namespace-Wordpress-Plugin
 Description: A plugin to add the podcasting 2.0 namespace to your Powerpress feeds
-Version: 1.1
+Version: 1.2
 Author: Lehmancreations
 Author URI: https://lehmancreations.com
 Requires at least: 3.6
@@ -61,8 +61,8 @@ class PodcastNamespace {
 				<b>To use the item level namespace place the following each in a custom field on the post:</b><br><br>
 			transcript is the name and value is a URL to the json encoded transcript file, you can have more than one of these but currently we only support json<br><br>
 			chapters is the name and value is a URL to a json encoded chapter file<br><br>
-			person is the name and value is the following each on its own line (name, host/guest, image url, other url to person)	<br><br>
-			soundbite is the name and value is the following each on its own line (title, start time, duration)
+			person: use the custom person box on the post	<br><br>
+			soundbite: use the custom soundbite box on the post
 				</p>
 </div>
 
@@ -145,6 +145,29 @@ class PodcastNamespace {
 			'podcast-namespace-admin', // page
 			'podcast_namespace_setting_section' // section
 		);
+		add_settings_field(
+			'location_description_0', // id
+			'Location Description', // title
+			array( $this, 'location_description_0_callback' ), // callback
+			'podcast-namespace-admin', // page
+			'podcast_namespace_setting_section' // section
+		);
+		
+		add_settings_field(
+			'location_geo_description_0', // id
+			'Location Coordinates Description', // title
+			array( $this, 'location_geo_description_0_callback' ), // callback
+			'podcast-namespace-admin', // page
+			'podcast_namespace_setting_section' // section
+		);
+		
+		add_settings_field(
+			'location_osm_description_0', // id
+			'Location OSMID Description', // title
+			array( $this, 'location_osm_description_0_callback' ), // callback
+			'podcast-namespace-admin', // page
+			'podcast_namespace_setting_section' // section
+		);
 	}
 
 	public function podcast_namespace_sanitize($input) {
@@ -175,7 +198,19 @@ class PodcastNamespace {
 		
 		if ( isset( $input['funding_description_0'] ) ) {
 			$sanitary_values['funding_description_0'] = sanitize_text_field( $input['funding_description_0'] );
+		}
+		
+		if ( isset( $input['location_description_0'] ) ) {
+			$sanitary_values['location_description_0'] = sanitize_text_field( $input['location_description_0'] );
+		}
+		
+		if ( isset( $input['location_geo_description_0'] ) ) {
+			$sanitary_values['location_geo_description_0'] = sanitize_text_field( $input['location_geo_description_0'] );
+		}
 
+		if ( isset( $input['location_osm_description_0'] ) ) {
+			$sanitary_values['location_osm_description_0'] = sanitize_text_field( $input['location_osm_description_0'] );
+			
 		return $sanitary_values;
 		}
 		
@@ -234,6 +269,27 @@ class PodcastNamespace {
 			isset( $this->podcast_namespace_options['funding_description_0'] ) ? esc_attr( $this->podcast_namespace_options['funding_description_0']) : ''
 		);
 	}
+		
+	public function location_description_0_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="podcast_namespace_option_name[location_description_0]" id="location_description_0" value="%s">',
+			isset( $this->podcast_namespace_options['location_description_0'] ) ? esc_attr( $this->podcast_namespace_options['location_description_0']) : ''
+		);
+	}
+	
+	public function location_geo_description_0_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="podcast_namespace_option_name[location_geo_description_0]" id="location_geo_description_0" value="%s">',
+			isset( $this->podcast_namespace_options['location_geo_description_0'] ) ? esc_attr( $this->podcast_namespace_options['location_geo_description_0']) : ''
+		);
+	}
+	
+	public function location_osm_description_0_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="podcast_namespace_option_name[location_osm_description_0]" id="location_osm_description_0" value="%s">',
+			isset( $this->podcast_namespace_options['location_osm_description_0'] ) ? esc_attr( $this->podcast_namespace_options['location_osm_description_0']) : ''
+		);
+	}
 
 }
 if ( is_admin() )
@@ -249,6 +305,9 @@ if ( is_admin() )
  * $advertising_contact_email_3 = $podcast_namespace_options['advertising_contact_email_3']; // Advertising Contact Email
  * $funding_url_4 = $podcast_namespace_options['funding_url_4']; // Funding Url
  * $funding_description_0 = $podcast_namespace_options['funding_description_0']; // Funding Description
+ * $location_description_0 = $podcast_namespace_options['location_description_0']; // Location Description
+ * $location_geo_description_0 = $podcast_namespace_options['location_geo_description_0']; // Location Description Coordinates
+ * $location_osm_description_0 = $podcast_namespace_options['location_osm_description_0']; // OSMID of the podcast
  */
 
 
@@ -280,7 +339,7 @@ function podastindex_rss2_head()
 	
 	
 	
-		echo "<!-- Podcast Namespace Tags Added by LehmanCreations V1.1 -->".PHP_EOL;	
+		echo "<!-- Podcast Namespace Tags Added by LehmanCreations V1.2 -->".PHP_EOL;	
 	
 	    if (!empty ( $podcast_namespace_options['locked_owner_1'] )) {
 			echo "\t".'<podcast:locked owner="' . $podcast_namespace_options['locked_owner_1'] .'">' . $podcast_namespace_options['locked_0'] . '</podcast:locked>'.PHP_EOL; }
@@ -296,6 +355,9 @@ function podastindex_rss2_head()
 		
 		if (!empty ( $podcast_namespace_options['funding_url_4'] )) {
 			echo "\t".'<podcast:funding url="'. $podcast_namespace_options['funding_url_4'] . '">' .$podcast_namespace_options['funding_description_0']. '</podcast:funding>'.PHP_EOL; }
+	
+	   if (!empty ( $podcast_namespace_options['location_description_0'] )) { 
+	  		echo "\t".'<podcast:location geo="geo:' . $podcast_namespace_options['location_geo_description_0'] . '" osm="' . $podcast_namespace_options['location_osm_description_0'] . '">' .$podcast_namespace_options['location_description_0']. '</podcast:location>'.PHP_EOL; }
 
 }
 
@@ -699,4 +761,250 @@ function podcastindex_rss2_soundbite( $content ) {
 
 
 add_filter( 'rss2_item', 'podcastindex_rss2_soundbite' );
+
+
+
+// Podcast Season
+// 
+// Add new meta box
+
+function podcast_season_meta_box() {
+	add_meta_box('podcast_season','Podcast Season and Episode ','podcast_season_callback','post');
+}
+add_action('add_meta_boxes','podcast_season_meta_box');
+
+function pdetails_callback( $post ) {
+
+	wp_nonce_field('podcast_season_save','podcast_season_meta_box_nonce');
+	$podcast_season =  get_post_meta($post->ID,'_podcast_season_key',false);
+}
+
+
+
+// create three new fields inside the box
+
+function podcast_season_callback( $post ) {
+
+	wp_nonce_field('podcast_season_save','podcast_season_meta_box_nonce');
+	$podcast_season =  get_post_meta($post->ID,'_podcast_season_key',false);
+
+	?>
+
+<table>
+	<th>Podcast Season</th>
+	<tr><td><label for="">Season Number</label></td>
+	<td>	<input type="number" name="season_number_field" placeholder="Season Number" value="<?php echo $podcast_season[0]['season_number']; ?>"></td></tr>
+   <tr> <td><label for="">Season Name (optional)</label></td>
+  <td>  <input type="text" name="season_name_field" placeholder="Enter Season Name" value="<?php echo $podcast_season[0]['season_name']; ?>"></td></tr>
+<!-- </table>
+
+
+<table> -->
+	<th>Podcast Episode</th>
+	<tr><td><label for="">Episode Number</label></td>
+	<td>	<input type="number" name="episode_number_field" placeholder="Episode Number" value="<?php echo $podcast_season[0]['episode_number']; ?>"></td></tr>
+   <tr> <td><label for="">Episode Name (optional)</label></td>
+  <td>  <input type="text" name="episode_name_field" placeholder="Enter Season Name" value="<?php echo $podcast_season[0]['episode_name']; ?>"></td></tr>
+</table>
+
+
+    <?php
+}
+
+
+
+
+
+//save metabox field values in the database
+
+function podcast_season_save( $post_id ) {
+
+	if( ! isset($_POST['podcast_season_meta_box_nonce'])) {
+		return;
+	}
+
+	if( ! wp_verify_nonce( $_POST['podcast_season_meta_box_nonce'], 'podcast_season_save') ) {
+		return;
+	}
+
+	if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if( ! current_user_can('edit_post', $post_id)) {
+		return;
+	}
+
+	$podcast_season_details = [
+	    'season_number' => $_POST['season_number_field'],
+        'season_name' => $_POST['season_name_field'],
+		'episode_number' => $_POST['episode_number_field'],
+		'episode_name' => $_POST['episode_name_field']
+    ];
+
+if ( $_POST['season_number_field'] == 0 AND $_POST['episode_number_field'] == 0) {
+	$podcast_season_items = get_post_meta($post->ID,'_podcast_season_key',true);
+		delete_post_meta($post_id,'_podcast_season_key', $podcast_season_items);
+	
+	} else {
+	update_post_meta( $post_id,'_podcast_season_key', $podcast_season_details );
+	}
+}
+add_action('save_post','podcast_season_save');
+//End Meta box
+
+//Season - rss function
+
+function podcastindex_rss2_season( $content ) {
+		if( !powerpress_is_podcast_feed() )
+		return;
+
+    global $post;
+    $podcast_season = get_post_meta($post->ID, '_podcast_season_key',false); // get the value 
+
+    // add soundbite only if the Custom Field is set
+  if ( $podcast_season ) {
+
+		if ($podcast_season[0]['season_name'] =="") {
+			
+		echo "\t".'<podcast:season>' . $podcast_season[0]['season_number'] . '</podcast:season>'.PHP_EOL;
+		} else {
+		echo "\t".'<podcast:season name="' . $podcast_season[0]['season_name'] . '">' . $podcast_season[0]['season_number'] . '</podcast:season>'.PHP_EOL;	
+		}		
+ 
+   } else {
+       return;
+   }
+}
+
+add_filter( 'rss2_item', 'podcastindex_rss2_season' );
+
+
+//Episode - rss function
+
+function podcastindex_rss2_episode( $content ) {
+		if( !powerpress_is_podcast_feed() )
+		return;
+
+    global $post;
+    $podcast_season = get_post_meta($post->ID, '_podcast_season_key',false); // get the value 
+
+    // add soundbite only if the Custom Field is set
+  if ( $podcast_season ) {
+
+		if ($podcast_season[0]['episode_name'] =="") {
+			
+		echo "\t".'<podcast:episode>' . $podcast_season[0]['episode_number'] . '</podcast:episode>'.PHP_EOL;
+		} else {
+		echo "\t".'<podcast:episode display="' . $podcast_season[0]['episode_name'] . '">' . $podcast_season[0]['episode_number'] . '</podcast:episode>'.PHP_EOL;	
+		}		
+ 
+   } else {
+       return;
+   }
+}
+
+add_filter( 'rss2_item', 'podcastindex_rss2_episode' );
+
+
+
+//Start Location
+// 
+// Add new meta box
+
+function podcast_location_meta_box() {
+	add_meta_box('podcast_location','Podcast Episode Location ','podcast_location_callback','post');
+}
+add_action('add_meta_boxes','podcast_location_meta_box');
+
+
+
+
+
+// create three new fields inside the box
+
+function podcast_location_callback( $post ) {
+
+	wp_nonce_field('podcast_location_save','podcast_location_meta_box_nonce');
+	$podcast_location =  get_post_meta($post->ID,'_podcast_location_key',false);
+
+	?>
+For more information about location see <a href="https://github.com/Podcastindex-org/podcast-namespace/blob/main/location/location.md" target="loc"> this document</a>
+<table>
+	<tr><td><label for="">Location Name (required)</label></td>
+	<td>	<input type="text" name="location_name_field" placeholder="Location Name" value="<?php echo $podcast_location[0]['location_name']; ?>"></td></tr>
+   <tr> <td><label for="">Lat/Long</label></td>
+  <td>  <input type="text" name="geo_field" placeholder="Coordinates" value="<?php echo $podcast_location[0]['lat_long_name']; ?>"></td></tr>
+  <tr> <td><label for="">OSMID</label></td>
+  <td>  <input type="text" name="osm_field" placeholder="OSMID" value="<?php echo $podcast_location[0]['osmid']; ?>"></td></tr>
+</table>
+
+
+    <?php
+}
+
+
+
+
+
+//save metabox field values in the database
+
+function podcast_location_save( $post_id ) {
+
+	if( ! isset($_POST['podcast_location_meta_box_nonce'])) {
+		return;
+	}
+
+	if( ! wp_verify_nonce( $_POST['podcast_location_meta_box_nonce'], 'podcast_location_save') ) {
+		return;
+	}
+
+	if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if( ! current_user_can('edit_post', $post_id)) {
+		return;
+	}
+
+	$podcast_location_details = [
+	    'location_name' => $_POST['location_name_field'],
+        'lat_long_name' => $_POST['geo_field'],
+        'osmid' => $_POST['osm_field']		
+    ];
+
+if ( $_POST['location_name_field'] == "") {
+	$podcast_location_items = get_post_meta($post->ID,'_podcast_location_key',true);
+		delete_post_meta($post_id,'_podcast_location_key', $podcast_location_items);
+	
+	} else {
+	update_post_meta( $post_id,'_podcast_location_key', $podcast_location_details );
+	}
+}
+add_action('save_post','podcast_location_save');
+
+//end meta box
+
+////Location - rss function
+
+function podcastindex_rss2_location( $content ) {
+		if( !powerpress_is_podcast_feed() )
+		return;
+
+    global $post;
+    $podcast_location = get_post_meta($post->ID, '_podcast_location_key',false); // get the value 
+
+    // add soundbite only if the Custom Field is set
+  if ( $podcast_location ) {
+
+		echo "\t".'<podcast:location geo="geo:'. $podcast_location[0]['lat_long_name'].'" osm="'. $podcast_location[0]['osmid'] . '">' . $podcast_location[0]['location_name'] . '</podcast:location>'.PHP_EOL;
+
+
+ 
+   } else {
+       return;
+   }
+}
+
+add_filter( 'rss2_item', 'podcastindex_rss2_location' );
 ?>
